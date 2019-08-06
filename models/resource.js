f2677e97175ba27c877b9b0e8186752d61971a8b
gosha <georgekagan@gmail.com>
2019-08-07 01:09:33 +0300

    validation

--- models/resource.js
+++ models/resource.js
@@ -3,7 +3,13 @@ const connection = require("../persistence/connection");
 const ResourceSchema = new connection.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique : true,
+        dropDups: true,
+        match : [
+            new RegExp('^[a-z0-9]+$', 'i'),
+            'Name must be alphanumeric'
+        ],
     },
     context: {
         type: String,
--- routes/resources.js
+++ routes/resources.js
@@ -3,18 +3,46 @@ const Resource = require('../models/resource');
 const router = express.Router();
 
 router.post('/:name', (req, res, next) => {
+    // Wasn't sure you wanted this implemented in a more robust way or a simple if would suffice
+    // More robust = a conditional rules set, e.g. something that Joi lib can provide, or similar
+    if (!req.body.ipRange && !req.body.location) {
+        res.status(403).json({error: 'resource must have either ipRange or location'});
+    }
+
     // I put name in the URI since this is the REST way
     const newResource = new Resource({
         name: req.params.name,
+        context: req.body.context,
     });
 
-    newResource.save().then(resource => res.json('added'));
+    newResource.save().then(resource => {
+        res.status(201).json(resource)
+    }).catch((err) => {
+        if (err.code === 11000) {
+            res.status(409).json({error: 'resource already exists'});
+        } else {
+            res.status(400).json({error: err.message});
+        }
+    });
 });
 
 router.get('/:name', (req, res, next) => {
     const name = req.params.name;
     const ip = req.query.ip;
-    res.send(`got ${name} ${ip}`);
+
+    if (!ip) {
+        return res.status(400).json({error: 'ip is required'});
+    }
+
+    Resource.find({name, ipRange: ip}).select({'context': 1, '_id': 0}).exec((err, item) => {
+        if (err) {
+            return next(err);
+        }
+        if (!item || !item.length) {
+            res.status(404).json({error: 'resource not found'});
+        }
+        res.json(item[0]);
+    });
 });
 
 module.exports = router;
