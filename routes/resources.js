8810dd615a073de2d7047feed5176bf198bc3200
gosha <georgekagan@gmail.com>
2019-08-07 01:35:34 +0300

    check ip range on read

--- routes/resources.js
+++ routes/resources.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Resource = require('../models/resource');
 const router = express.Router();
+const ipRangeCheck = require("ip-range-check");
+
+const DB_ERR_DUPLICATE = 11000;
 
 router.post('/:name', (req, res, next) => {
     // Wasn't sure you wanted this implemented in a more robust way or a simple if would suffice
@@ -13,12 +16,14 @@ router.post('/:name', (req, res, next) => {
     const newResource = new Resource({
         name: req.params.name,
         context: req.body.context,
+        ipRange: req.body.ipRange,
+        location: req.body.location
     });
 
     newResource.save().then(resource => {
         res.status(201).json(resource)
     }).catch((err) => {
-        if (err.code === 11000) {
+        if (err.code === DB_ERR_DUPLICATE) {
             res.status(409).json({error: 'resource already exists'});
         } else {
             res.status(400).json({error: err.message});
@@ -34,14 +39,17 @@ router.get('/:name', (req, res, next) => {
         return res.status(400).json({error: 'ip is required'});
     }
 
-    Resource.find({name, ipRange: ip}).select({'context': 1, '_id': 0}).exec((err, item) => {
+    Resource.find({name}).select({'context': 1, 'ipRange': 1, '_id': 0}).exec((err, resource) => {
         if (err) {
             return next(err);
         }
-        if (!item || !item.length) {
-            res.status(404).json({error: 'resource not found'});
+        if (!resource || !resource.length) {
+            return res.status(404).json({error: 'resource not found'});
         }
-        res.json(item[0]);
+        if (!ipRangeCheck(ip, resource[0].ipRange)) {
+            return res.status(403).json({error: 'forbidden'});
+        }
+        res.json({context: resource[0].context});
     });
 });
 
